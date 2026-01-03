d9280f1a4853795c2cafb83b70ac7a1b01ef40ea
nekopunch <taiikichan@naver.com>
2026-01-03 15:15:34 +0900

    work

---
+++ back/src/main/java/com/back/App1Test260103Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App1Test260103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App1Test260103Application.class, args);
+	}
+
+}
